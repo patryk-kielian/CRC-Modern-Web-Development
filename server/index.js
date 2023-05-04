ac2bd9fe747f19c97b175a25f8ec61508dc7d746
Patryk Kielian <patryk.kielian0@gmail.com>
2023-05-04 20:00:55 +0200

    db connection

--- server/index.js
+++ server/index.js
@@ -5,12 +5,30 @@ const app = express();
 
 app.use(express.json());
 
-const db = mysql.createConnection;
-({
+const db = mysql.createConnection({
   user: "root",
-  host: "localhost",
-  password: "password",
-  database: "LoginSystem",
+  password: "WelcomOnCRC23",
+  database: "crc_db",
 });
+
+app.get("/", (req, res) => {
+  const sqlInsert =
+    "INSERT INTO users (login, password) VALUES ('user', 'test')";
+  db.query(sqlInsert, (err, result) => {
+    if (err) throw err;
+    res.send("hello world");
+  });
+});
+
+// app.get("/init", (req, res) => {
+//   const sqlQuery =
+//     "CREATE TABLE IF NOT EXISTS emails(id int AUTO_INCREMENT, firstname VARCHAR(50), lastname VARCHAR(50), email VARCHAR(50), PRIMARY KEY(id))";
+
+//   db.query(sqlQuery, (err) => {
+//     if (err) throw err;
+
+//     res.send("Table created!");
+//   });
+// });
 
 app.listen(3001, () => console.log("running server"));
