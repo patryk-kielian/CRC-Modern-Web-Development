05f4569791560bf3f94991e3d42e86c9b2432401
Patryk Kielian <patryk.kielian0@gmail.com>
2023-05-16 14:18:50 +0200

    App test - routing

--- client/src/tests/App.test.jsx
+++ client/src/tests/App.test.jsx
@@ -1,11 +1,50 @@
-import { describe, it, expect } from "vitest";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import App from "../App";
+import { expect } from "vitest";
 
-describe("something truthy and falsy", () => {
-  it("true to be true", () => {
-    expect(true).toBe(true);
+describe("App", () => {
+  test("renders Home component for the '/' route", () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(
+      screen.getByText("Don’t wait - start training now!")
+    ).toBeInTheDocument();
   });
 
-  it("false to be false", () => {
-    expect(false).toBe(false);
+  test("renders User component for the '/user' route", () => {
+    render(
+      <MemoryRouter initialEntries={["/user"]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(
+      screen.getByText("This page is only for logged in users")
+    ).toBeInTheDocument();
+  });
+
+  test("renders CreateNewTraining component for the '/create-new-training' route", () => {
+    render(
+      <MemoryRouter initialEntries={["/create-new-training"]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(
+      screen.getByText("You must have admin permissions to view this page")
+    ).toBeInTheDocument();
+  });
+
+  test("renders Login component for the '/login' route", () => {
+    render(
+      <MemoryRouter initialEntries={["/login"]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText("Welcome back!")).toBeInTheDocument();
   });
 });
