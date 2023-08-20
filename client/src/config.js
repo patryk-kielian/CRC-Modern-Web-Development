90367cdf36d0f1a795dc268435b84472f5d0983f
Patryk Kielian <patryk.kielian0@gmail.com>
2023-08-20 16:45:34 +0200

    responsive Course, Course Learn build

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 // TODO: change back from local to hosted
-// const API_URL = "https://crc-modern-web-development-api.vercel.app";
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "https://crc-modern-web-development-api.vercel.app";
+// const API_URL = "http://127.0.0.1:5000";
 
 export { API_URL };
