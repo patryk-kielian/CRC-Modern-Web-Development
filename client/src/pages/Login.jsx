c14cf4e2ba0f37857b27d44928d7a8b60252d112
Patryk Kielian <patryk.kielian0@gmail.com>
2023-08-21 18:37:07 +0200

    improved Error handling, trailing spaces Login fix

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -6,6 +6,7 @@ import "../styles/Login.css";
 
 import { LoggedUserContext } from "../contexts/LoggedUserContext";
 import Popup from "../components/Popup";
+import Error from "./Error";
 
 function Login() {
   const { mode } = useParams();
@@ -18,7 +19,7 @@ function Login() {
   const [message, setMessage] = useState("");
   const [showPopup, setShowPopup] = useState(false);
 
-  const { setLoggedUser } = useContext(LoggedUserContext);
+  const { loggedUser, setLoggedUser } = useContext(LoggedUserContext);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -30,23 +31,27 @@ function Login() {
     Axios.post(`${API_URL}/login`, {
       username: username,
       password: password,
-    }).then((response) => {
-      console.log(response);
-      if (response.data.message) {
-        setMessage(response.data.message);
+    })
+      .then((response) => {
+        if (response.data.message) {
+          setMessage(response.data.message);
+          setShowPopup(true);
+        } else {
+          setMessage("Succesfully logged in!");
+          setShowPopup(true);
+          const token = response.data.token;
+          // Store the token in localStorage for persistence
+          localStorage.setItem("jwtToken", token);
+          Axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+          setLoggedUser(response.data.user);
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        setMessage(error.message);
         setShowPopup(true);
-      } else {
-        setMessage("Succesfully logged in!");
-        setShowPopup(true);
-        const token = response.data.token;
-        console.log(token);
-        // Store the token in localStorage for persistence
-        localStorage.setItem("jwtToken", token);
-        Axios.defaults.headers.common["Authorization"] = "Bearer " + token;
-        setLoggedUser(response.data.user);
-        navigate("/");
-      }
-    });
+      });
   };
 
   const registerUser = (e) => {
@@ -79,97 +84,119 @@ function Login() {
     });
   };
 
+  /**
+   * Trims the user inputs in order to prevent submitting usernames and passwords with trailing spaces
+   */
+  const trimInputs = () => {
+    setUsername((prev) => prev.trimEnd());
+    setPassword((prev) => prev.trimEnd());
+    setPasswordCheck((prev) => prev.trimEnd());
+  };
+
   return (
     <>
-      <main className="login-background">
-        <div className="login-content">
-          <h2>{registerMode ? "Create an account" : "Welcome back!"}</h2>
-          <form className="login-form">
-            <div className="login-form-inputs">
-              <div className="login-form-input-block">
-                <label htmlFor="login">
-                  {registerMode ? "Username:" : "Login:"}
-                </label>
+      {!loggedUser ? (
+        <main className="login-background">
+          <div className="login-content">
+            <h2>{registerMode ? "Create an account" : "Welcome back!"}</h2>
+            <form className="login-form">
+              <div className="login-form-inputs">
+                <div className="login-form-input-block">
+                  <label htmlFor="login">
+                    {registerMode ? "Username:" : "Login:"}
+                  </label>
 
+                  <input
+                    type="text"
+                    id="login"
+                    name="login"
+                    placeholder="Type username"
+                    value={username}
+                    onChange={(e) => {
+                      setUsername(e.target.value);
+                    }}
+                    onBlur={trimInputs}
+                  />
+                </div>
+                <div className="login-form-input-block">
+                  <label htmlFor="password">Password:</label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Type password"
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                    onBlur={trimInputs}
+                  />
+                </div>
+                <div className="login-form-input-block">
+                  {registerMode ? (
+                    <>
+                      <label htmlFor="password">Confirm Password:</label>
+                      <input
+                        type="password"
+                        id="passwordCheck"
+                        name="passwordCheck"
+                        placeholder="Repeat password"
+                        value={passwordCheck}
+                        onChange={(e) => {
+                          setPasswordCheck(e.target.value);
+                        }}
+                      />
+                    </>
+                  ) : (
+                    <p className="login-tip">
+                      Tip: To have the ability to create and manage tutorials,
+                      use the following credentials:
+                      <br />
+                      username: admin
+                      <br />
+                      password: test
+                    </p>
+                  )}
+                </div>
+              </div>
+              <div className="login-buttons">
                 <input
-                  type="text"
-                  id="login"
-                  name="login"
-                  placeholder="Type username"
-                  value={username}
-                  onChange={(e) => {
-                    setUsername(e.target.value);
-                  }}
+                  className="violet login-button"
+                  type="submit"
+                  value={registerMode ? "Register" : "Log in"}
+                  onClick={registerMode ? registerUser : loginUser}
                 />
-              </div>
-              <div className="login-form-input-block">
-                <label htmlFor="password">Password:</label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  placeholder="Type password"
-                  value={password}
-                  onChange={(e) => {
-                    setPassword(e.target.value);
+                <button
+                  className="ghost-black register-account-button"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setRegisterMode(!registerMode);
+                    setUsername("");
+                    setPassword("");
+                    setPasswordCheck("");
                   }}
-                />
+                >
+                  <span className="fine-button">
+                    {registerMode
+                      ? "Already have an account? "
+                      : "No account yet? "}
+                  </span>{" "}
+                  {registerMode ? "Login" : "Register"}
+                </button>
               </div>
-              <div className="login-form-input-block">
-                {registerMode ? (
-                  <>
-                    <label htmlFor="password">Confirm Password:</label>
-                    <input
-                      type="password"
-                      id="passwordCheck"
-                      name="passwordCheck"
-                      placeholder="Repeat password"
-                      value={passwordCheck}
-                      onChange={(e) => {
-                        setPasswordCheck(e.target.value);
-                      }}
-                    />
-                  </>
-                ) : (
-                  <p className="login-tip">
-                    Tip: To have the ability to create and manage tutorials, use
-                    the following credentials:
-                    <br />
-                    username: admin
-                    <br />
-                    password: test
-                  </p>
-                )}
-              </div>
-            </div>
-            <div className="login-buttons">
-              <input
-                className="violet login-button"
-                type="submit"
-                value={registerMode ? "Register" : "Log in"}
-                onClick={registerMode ? registerUser : loginUser}
-              />
-              <button
-                className="ghost-black register-account-button"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setRegisterMode(!registerMode);
-                  setUsername("");
-                  setPassword("");
-                  setPasswordCheck("");
-                }}
-              >
-                <span className="fine-button">
-                  {registerMode
-                    ? "Already have an account? "
-                    : "No account yet? "}
-                </span>{" "}
-                {registerMode ? "Login" : "Register"}
-              </button>
-            </div>
-          </form>
-        </div>
-      </main>
+            </form>
+          </div>
+        </main>
+      ) : (
+        <Error
+          textMessage={
+            "You are already logged in! If you want to change users, log out first"
+          }
+          textButton={"Back to Home"}
+          route={"/"}
+        />
+      )}
+
       <Popup
         message={message}
         showPopup={showPopup}
